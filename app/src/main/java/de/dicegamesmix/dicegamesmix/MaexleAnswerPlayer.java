d80e23fd2ca735b19856803a6d15c8070bef5adc
codeffeineMate <ShopUndInfo@online.de>
2018-08-11 00:55:24 +0200

    Started to build my two game screens, LAYOUT
    in both files a button to go further is missing

--- app/src/main/java/de/dicegamesmix/dicegamesmix/MaexleAnswerPlayer.java
+++ app/src/main/java/de/dicegamesmix/dicegamesmix/MaexleAnswerPlayer.java
@@ -2,6 +2,8 @@ package de.dicegamesmix.dicegamesmix;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MaexleAnswerPlayer extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MaexleAnswerPlayer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maexle_answer_player);
+
+        //object for the graphical elements
+        Button maexle_ansPl_menu_btn = (Button) findViewById(R.id.maexle_ansPl_menu_btn);
+        TextView maexle_ansPl_playerName_tv = (TextView) findViewById(R.id.maexle_answPl_playerName_tv);
+        //TextView maexle_ansPl_
+
+
+        /*notes:
+                - answerPlayer: Ok button fehlt
+                - answerUncover: weiter button fehlt
+
+                ALLG: Programmlogik mit Tim besprechen!!
+         */
     }
 }
