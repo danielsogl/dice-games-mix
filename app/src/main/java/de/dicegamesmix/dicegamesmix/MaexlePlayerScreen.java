b2343d114695ba49af4b9998d550d34426d2e1b1
Unknown <tturek@mailbox.org>
2018-08-10 12:34:52 +0200

    Player Screen Ready

--- app/src/main/java/de/dicegamesmix/dicegamesmix/MaexlePlayerScreen.java
+++ app/src/main/java/de/dicegamesmix/dicegamesmix/MaexlePlayerScreen.java
@@ -1,11 +1,14 @@
 package de.dicegamesmix.dicegamesmix;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,10 +30,19 @@ public class MaexlePlayerScreen extends AppCompatActivity {
         playerList = Paper.book().read("Players", new ArrayList<String>());
 
         final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.playerscreen_listview_layout, playerList);
-        ListView maexle_player_playerList = (ListView) findViewById(R.id.maexle_player_playerList);
+        final ListView maexle_player_playerList = (ListView) findViewById(R.id.maexle_player_playerList);
         maexle_player_playerList.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
 
-        Button maexle_player_btn_add = (Button) findViewById(R.id.maexle_player_btn_add);
+        Button start_game_btn = (Button) findViewById(R.id.start_game_btn);
+        start_game_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent maexle_Gamescreen = new Intent(getApplicationContext(), MaexleGameScreen.class);
+                startActivity(maexle_Gamescreen);
+            }
+        });
+        final Button maexle_player_btn_add = (Button) findViewById(R.id.maexle_player_btn_add);
         maexle_player_btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,6 +70,41 @@ public class MaexlePlayerScreen extends AppCompatActivity {
                 AlertDialog dialog = playername.create();
                 dialog.show();
 
+                /*maexle_player_playerList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        playerList.remove(i);
+                        adapter.notifyDataSetChanged();
+                        Paper.book().write("Players", playerList);
+                        return true;
+                    }
+                });*/
+
+                maexle_player_playerList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapterView, View view, final int p, long l) {
+                        AlertDialog.Builder deletePlayer = new AlertDialog.Builder(MaexlePlayerScreen.this);
+                        deletePlayer.setMessage("Are you sure you want to delete this player?")
+                                .setTitle("delete player")
+                                .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        playerList.remove(p);
+                                        adapter.notifyDataSetChanged();
+                                        Paper.book().write("Players", playerList);
+                                    }
+                                })
+                                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        dialogInterface.dismiss();
+                                    }
+                                });
+                        AlertDialog deleteDialog = deletePlayer.create();
+                        deleteDialog.show();
+                        return true;
+                    }
+                });
 
             }
         });
