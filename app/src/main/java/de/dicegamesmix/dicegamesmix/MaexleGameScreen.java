14ab1017cc213e753c0806ea6f6420bd54f8f5ed
TimT <tturek@mailbox.org>
2018-08-10 16:47:39 +0200

    player screen improved, game screen basics
    the basics of the game screen are not important, because of the fact, that we will do instead of one activity four activities.

--- app/src/main/java/de/dicegamesmix/dicegamesmix/MaexleGameScreen.java
+++ app/src/main/java/de/dicegamesmix/dicegamesmix/MaexleGameScreen.java
@@ -2,6 +2,10 @@ package de.dicegamesmix.dicegamesmix;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MaexleGameScreen extends AppCompatActivity {
 
@@ -9,6 +13,13 @@ public class MaexleGameScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maexle_game_screen);
+
+        TextView game_result = (TextView) findViewById(R.id.game_result);
+        TextView game_result_number = (TextView) findViewById(R.id.game_result_number);
+        TextView game_answer = (TextView) findViewById(R.id.game_answer);
+        EditText game_answer_player = (EditText) findViewById(R.id.game_answer_player);
+        Button game_roll_btn = (Button) findViewById(R.id.game_roll_btn);
+        Button game_uncover_btn = (Button) findViewById(R.id.game_uncover_btn);
 
         //place where the game takes place
     }
