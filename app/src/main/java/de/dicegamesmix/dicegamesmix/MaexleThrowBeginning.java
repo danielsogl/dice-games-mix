362948e95b715f719380fb3810eb912fdf110130
TimT <tturek@mailbox.org>
2018-08-11 09:33:46 +0200

    started to build other two gamescreens

--- app/src/main/java/de/dicegamesmix/dicegamesmix/MaexleThrowBeginning.java
+++ app/src/main/java/de/dicegamesmix/dicegamesmix/MaexleThrowBeginning.java
@@ -1,7 +1,11 @@
 package de.dicegamesmix.dicegamesmix;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MaexleThrowBeginning extends AppCompatActivity {
 
@@ -9,5 +13,33 @@ public class MaexleThrowBeginning extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maexle_throw_beginning);
+
+        TextView beginning_player_name = (TextView) findViewById(R.id.beginning_player_name);
+        TextView beginning_result = (TextView) findViewById(R.id.beginning_result);
+        final TextView beginning_result_number = (TextView) findViewById(R.id.beginning_result_number);
+        Button beginning_throw_btn = (Button) findViewById(R.id.beginning_throw_btn);
+        final Integer throw1 = (int) (Math.random()*6)+1;
+        final Integer throw2 = (int) (Math.random()*6)+1;
+        final Intent maexle_answerPlayer = new Intent(getApplicationContext(), MaexleAnswerPlayer
+        .class);
+        final Integer result = 0; //variabel for saving the result of the dice from this screen. have to test how to programm so it
+        //works like ist should work. has to be after each of the if-conditions.
+
+        beginning_throw_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(throw1==throw2){
+                    beginning_result_number.setText(throw1.toString()+throw2.toString());
+                }
+                if(throw1>throw2){
+                    beginning_result_number.setText(throw1.toString()+throw2.toString());
+                }
+                else if(throw1<throw2){
+                    beginning_result_number.setText(throw2.toString()+throw1.toString());
+                }
+                maexle_answerPlayer.putExtra("de.dicegamesmix.dicegamesmix.result", result);
+            }
+        });
+        beginning_player_name.setText(result);
     }
 }
--- app/src/main/java/de/dicegamesmix/dicegamesmix/MaexleThrowMiddle.java
+++ app/src/main/java/de/dicegamesmix/dicegamesmix/MaexleThrowMiddle.java
@@ -1,7 +1,10 @@
 package de.dicegamesmix.dicegamesmix;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.widget.Button;
+        import android.widget.TextView;
 
 public class MaexleThrowMiddle extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MaexleThrowMiddle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maexle_throw_middle);
+
+        TextView middle_player_name = (TextView) findViewById(R.id.middle_player_name);
+        TextView middle_result = (TextView) findViewById(R.id. middle_result);
+        TextView middle_result_number = (TextView) findViewById(R.id.middle_result_number);
+        Button middle_uncover_btn = (Button) findViewById(R.id.middle_uncover_btn);
+        Button middle_throw_btn = (Button) findViewById(R.id.middle_throw_btn);
+        Intent maexle_answerPlayer = new Intent(getApplicationContext(), MaexleAnswerPlayer.class);
+        Intent maexle_answerUncover = new Intent(getApplicationContext(), MaexleAnswerUncover.class);
     }
 }
