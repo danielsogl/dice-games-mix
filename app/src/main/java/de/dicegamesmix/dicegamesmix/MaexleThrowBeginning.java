cc78c161ce552b74760f4d22d0aad0d7a14b84e2
TimT <tturek@mailbox.org>
2018-08-11 12:35:49 +0200

    update

--- app/src/main/java/de/dicegamesmix/dicegamesmix/MaexleThrowBeginning.java
+++ app/src/main/java/de/dicegamesmix/dicegamesmix/MaexleThrowBeginning.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.List;
+
+import io.paperdb.Paper;
+
 public class MaexleThrowBeginning extends AppCompatActivity {
 
     @Override
@@ -14,32 +18,55 @@ public class MaexleThrowBeginning extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maexle_throw_beginning);
 
+        //declaration
         TextView beginning_player_name = (TextView) findViewById(R.id.beginning_player_name);
         TextView beginning_result = (TextView) findViewById(R.id.beginning_result);
         final TextView beginning_result_number = (TextView) findViewById(R.id.beginning_result_number);
         Button beginning_throw_btn = (Button) findViewById(R.id.beginning_throw_btn);
-        final Integer throw1 = (int) (Math.random()*6)+1;
-        final Integer throw2 = (int) (Math.random()*6)+1;
+        Button beginning_back_btn = (Button) findViewById(R.id.beginning_back_btn);
+        final Integer throw1 = (int) (Math.random() * 6) + 1;
+        final Integer throw2 = (int) (Math.random() * 6) + 1;
+        int index = 0;
+
         final Intent maexle_answerPlayer = new Intent(getApplicationContext(), MaexleAnswerPlayer
-        .class);
-        final Integer result = 0; //variabel for saving the result of the dice from this screen. have to test how to programm so it
-        //works like ist should work. has to be after each of the if-conditions.
+                .class);
+        final Intent back_to_menu = new Intent(getApplicationContext(),MainActivity.class);
+
+        //read index
+        index = Paper.book().read("index");
+
+        final String resultN = throw1.toString() + throw2.toString();
+        //import all names
+        List<String> playerNames = Paper.book().read("Players");
+
+        //take the one you need with the index
+        String playername = playerNames.get(index);
+        beginning_player_name.setText(playername);
 
         beginning_throw_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(throw1==throw2){
-                    beginning_result_number.setText(throw1.toString()+throw2.toString());
+                if (throw1 == throw2) {
+                    beginning_result_number.setText(throw1.toString() + throw2.toString());
+                    Paper.book().write("result", resultN);
                 }
-                if(throw1>throw2){
-                    beginning_result_number.setText(throw1.toString()+throw2.toString());
+                else if (throw1 > throw2) {
+                    beginning_result_number.setText(throw1.toString() + throw2.toString());
+                    Paper.book().write("result", resultN);
+                } else {
+                    beginning_result_number.setText(throw2.toString() + throw1.toString());
+                    Paper.book().write("result", resultN);
                 }
-                else if(throw1<throw2){
-                    beginning_result_number.setText(throw2.toString()+throw1.toString());
-                }
-                maexle_answerPlayer.putExtra("de.dicegamesmix.dicegamesmix.result", result);
             }
         });
-        beginning_player_name.setText(result);
+
+        beginning_back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(back_to_menu);
+            }
+        });
+        //put in delay or display
+
     }
 }
--- app/src/main/java/de/dicegamesmix/dicegamesmix/MaexleThrowMiddle.java
+++ app/src/main/java/de/dicegamesmix/dicegamesmix/MaexleThrowMiddle.java
@@ -3,8 +3,13 @@ package de.dicegamesmix.dicegamesmix;
         import android.content.Intent;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.View;
         import android.widget.Button;
         import android.widget.TextView;
+
+        import java.util.List;
+
+        import io.paperdb.Paper;
 
 public class MaexleThrowMiddle extends AppCompatActivity {
 
@@ -14,11 +19,48 @@ public class MaexleThrowMiddle extends AppCompatActivity {
         setContentView(R.layout.activity_maexle_throw_middle);
 
         TextView middle_player_name = (TextView) findViewById(R.id.middle_player_name);
-        TextView middle_result = (TextView) findViewById(R.id. middle_result);
+        TextView middle_answer = (TextView) findViewById(R.id. middle_answer);
         TextView middle_result_number = (TextView) findViewById(R.id.middle_result_number);
         Button middle_uncover_btn = (Button) findViewById(R.id.middle_uncover_btn);
         Button middle_throw_btn = (Button) findViewById(R.id.middle_throw_btn);
-        Intent maexle_answerPlayer = new Intent(getApplicationContext(), MaexleAnswerPlayer.class);
-        Intent maexle_answerUncover = new Intent(getApplicationContext(), MaexleAnswerUncover.class);
+        Button middle_back_btn = (Button) findViewById(R.id.middle_back_btn);
+
+        int index = 0;
+
+        final Intent maexle_answerPlayer = new Intent(getApplicationContext(), MaexleAnswerPlayer.class);
+        final Intent maexle_answerUncover = new Intent(getApplicationContext(), MaexleAnswerUncover.class);
+        final Intent back_to_menu = new Intent(getApplicationContext(), MainActivity.class);
+
+        //read index
+        index = Paper.book().read("index");
+
+        //import all names
+        List<String> playerNames = Paper.book().read("Players");
+        //take the one you need with the index
+        String playername = playerNames.get(index);
+        middle_player_name.setText(playername);
+
+        middle_answer.setText("answer from"+ playerNames.get(index-1));
+
+        middle_uncover_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(maexle_answerUncover);
+            }
+        });
+
+        middle_throw_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(maexle_answerPlayer);
+            }
+        });
+
+        middle_back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(back_to_menu);
+            }
+        });
     }
 }
